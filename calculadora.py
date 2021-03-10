c466e80b00d8250526f8fe3671f5c88e528e07cb
Guilherme Santos <guipinela.santos@gmail.com>
2021-03-10 19:04:18 +0000

    1 º commit
    Processo de iniciação de variáveis e interação com o usuário

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+print()
+print('{:-^40}'.format('Calculadora'))
+print()
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+print("""Escolha uma diz opções:\n
+Prima - [1] - Somar
+Prima - [2] - Subtrair
+Prima - [3] - Multiplicar
+Prima - [4] - Potência de 2
+Prima - [5] - Adicionar outro número\n""")
+opcao = int(input('Digite a sua opção pretendida: '))

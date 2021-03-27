65da98a3ed9fe42868061c0185af77a9d3ff92aa
Guilherme Santos <guipinela.santos@gmail.com>
2021-03-27 21:22:39 +0000

    commit

--- subtracao.py
+++ subtracao.py
@@ -1,9 +1,7 @@
 listanum = []
+sub = 0
 listanum.append(int(input('Digite um numero: ')))
 listanum.append(int(input('Digite outro numero: ')))
-numsub = listanum[0]
-print(numsub)
 for num in listanum:
-    print(num)
-    numsub -= num
-print(numsub)
+    sub -= num
+print(sub)

7323a980bfa60b53c7f8507967cd5d62a5ee6b03
Guilherme Santos <guipinela.santos@gmail.com>
2021-04-01 19:36:15 +0100

    commit

--- calculadora_add.py
+++ calculadora_add.py
@@ -15,39 +15,40 @@ while True:
         continuar = ' '
         if continuar not in 'SN':
             continuar = str(input('Quer continuar a digitar mais números [S/N]: ')).upper().strip()[0]
-        if continuar == 'N':
-            print('-' * 60)
-            print('''Escolha uma das seguintes opções:
-        [1] - Somar
-        [2] - Subtrair
-        [3] - Multiplicar
-        [4] - Divisão
-        [5] - Sair do programa''')
-            try:
-                opcao = int(input('Digite o sua opção: '))
-            except:
-                print('\033[1;31mErro digite um número inteiro!\033[m')
-            else:
+        elif continuar == 'N':
+            while True:
                 print('-' * 60)
-                if opcao == 5:
-                    break
-                elif opcao == 1:
-                    for num in listanum:
-                        soma += num
-                    print(f'O resultado é igual a \033[32m{soma}\033[m')
-                elif opcao == 2:
-                    for num in listanum:
-                        subtracao -= num
-                    print(f'O resultado é igual a \033[32m{subtracao}\033[m')
-                elif opcao == 3:
-                    mult = 1
-                    for num in listanum:
-                        mult *= num
-                    print(f'O resultado é igual a \033[32m{mult}\033[m')
-                elif opcao == 4:
-                    div = 0
-                    for num in listanum:
-                        div /= num
-                    print(f'O resultado é igual a \033[32m{div}\033[m')
+                print('''Escolha uma das seguintes opções:
+            [1] - Somar
+            [2] - Subtrair
+            [3] - Multiplicar
+            [4] - Divisão
+            [5] - Sair do programa''')
+                try:
+                    opcao = int(input('Digite o sua opção: '))
+                except:
+                    print('\033[1;31mErro digite um número inteiro!\033[m')
+                else:
+                    print('-' * 60)
+                    if opcao == 5:
+                        break
+                    elif opcao == 1:
+                        for num in listanum:
+                            soma += num
+                        print(f'O resultado é igual a \033[32m{soma}\033[m')
+                    elif opcao == 2:
+                        for num in listanum:
+                            subtracao -= num
+                        print(f'O resultado é igual a \033[32m{subtracao}\033[m')
+                    elif opcao == 3:
+                        mult = 1
+                        for num in listanum:
+                            mult *= num
+                        print(f'O resultado é igual a \033[32m{mult}\033[m')
+                    elif opcao == 4:
+                        div = 0
+                        for num in listanum:
+                            div /= num
+                        print(f'O resultado é igual a \033[32m{div}\033[m')
     elif tiponumeros == 'reais':
         print('Ola, mundo')

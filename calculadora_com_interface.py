b2db75eb1891061bcad6dc81dbe857171a8cbfc7
Guilherme Santos <guipinela.santos@gmail.com>
2021-04-08 14:04:49 +0100

    Update calculadora_com_interface.py

--- calculadora_com_interface.py
+++ calculadora_com_interface.py
@@ -19,7 +19,8 @@ btn_7 = tk.Button(text='7', width=5, relief=tk.GROOVE, borderwidth=2, bg='white'
 btn_8 = tk.Button(text='8', width=5, relief=tk.GROOVE, borderwidth=2, bg='white')
 btn_9 = tk.Button(text='9', width=5, relief=tk.GROOVE, borderwidth=2, bg='white')
 
-# Botões de operação (x - multiplacação)
+# Botões de operação
+# (x - multiplacação)
 # (s - subtração -)
 # (a - adição +)
 # (i - igual =)
@@ -27,6 +28,12 @@ btn_x = tk.Button(text='x', width=5, relief=tk.GROOVE, borderwidth=2, bg='#C4CBC
 btn_s = tk.Button(text='-', width=5, relief=tk.GROOVE, borderwidth=2, bg='#C4CBCA')
 btn_a = tk.Button(text='+', width=5, relief=tk.GROOVE, borderwidth=2, bg='#C4CBCA')
 btn_i = tk.Button(text='=', width=5, relief=tk.GROOVE, borderwidth=2, bg='#66C4F2')
+
+# Outros Botões
+# (n - positvo ou negativo)
+# (v - vígula)
+btn_n = tk.Button(text='+/-', width=5, relief=tk.GROOVE, borderwidth=2, bg='#C4CBCA')
+btn_v = tk.Button(text=',', width=5, relief=tk.GROOVE, borderwidth=2, bg='#C4CBCA')
 
 # Botões numéricos posicionados
 btn_0.grid(row=3, column=1, ipady=5, padx=2)
@@ -40,10 +47,14 @@ btn_7.grid(row=0, column=0, ipady=5, padx=2)
 btn_8.grid(row=0, column=1, ipady=5, padx=2)
 btn_9.grid(row=0, column=2, ipady=5, padx=2)
 
-# Botões de operação
+# Botões de operação posicionados
 btn_x.grid(row=0, column=3, ipady=5, padx=2)
 btn_s.grid(row=1, column=3, ipady=5, padx=2)
 btn_a.grid(row=2, column=3, ipady=5, padx=2)
 btn_i.grid(row=3, column=3, ipady=5, padx=2)
+
+# Outros Botões posicionados
+btn_n.grid(row=3, column=0, ipady=5, padx=2)
+btn_v.grid(row=3, column=2, ipady=5, padx=2)
 
 janela_principal.mainloop()

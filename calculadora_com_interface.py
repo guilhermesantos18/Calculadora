bd36eadbba017738946aeb1d31df22de4780a413
Guilherme Santos <guipinela.santos@gmail.com>
2021-04-08 11:38:05 +0100

    commit

--- calculadora_com_interface.py
+++ calculadora_com_interface.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 
+# Configurações da janela principal, tamanho, titulo, configurações
+# das linha, e das colunas
 janela_principal = tk.Tk()
+janela_principal.geometry('250x200')
+janela_principal.title('Calculadora')
+janela_principal.rowconfigure([0, 1, 2], minsize=100)
 
-for butao in range(1, 10):
-    btn = tk.Button(text=f'{butao}')
-    for linha in range(1, 4):
-        for coluna in range(1, 4):
-            btn.grid(row=linha, column=coluna)
+frame_butao = tk.Frame(master=janela_principal, relief=tk.FLAT, borderwidth=2)
+
+btn_1 = tk.Button(master=janela_principal, text='1', width=5)
+
+btn_1.grid(row=1, column=0, ipady=5, padx=20)
+
 janela_principal.mainloop()
